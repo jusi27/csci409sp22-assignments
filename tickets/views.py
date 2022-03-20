89bb44f3fe9740378ac5d704a3bee631c06f9827
jusi27 <jbsisler@coastal.edu>
2022-03-19 22:26:56 -0400

    Week 9 Assignment

--- tickets/views.py
+++ tickets/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
+from .models import Reservation
 
 
 def index(request):
-    return HttpResponse('Hello from tickets');
+    # Nothing to do here
+    return HttpResponse('Hello from tickets')
 
 
 def ticket_search(request, confirmation_number):
-    return HttpResponse('Showing tickets for confirmation number: ' + str(confirmation_number));
+    # Select the singular reservation for the confirmation number
+    # Note: the confirmation_number is the id in the Reservation table
+    reservation = Reservation.objects.get(id__exact=confirmation_number)
+    return HttpResponse(
+        'Number of tickets for confirmation number: ' + str(confirmation_number) + " is " + str(reservation.num_people))

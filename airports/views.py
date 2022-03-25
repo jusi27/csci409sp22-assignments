75e9090acd199715e351b3704734f3a6af8eb1e7
jusi27 <jbsisler@coastal.edu>
2022-03-25 17:09:35 -0400

    Week 10 Assignment

--- airports/views.py
+++ airports/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Airport
 
 
@@ -7,8 +8,10 @@ def index(request):
     airports = Airport.objects.all()
     # Create a displayable string. We will change this next week.
     # This is just to show some data.
-    airport_list = ', '.join([a.airport_code for a in airports])
-    return HttpResponse('Showing all airports: ' + airport_list);
+    #airport_list = ', '.join([a.airport_code for a in airports])
+    #return HttpResponse('Showing all airports: ' + airport_list)
+    context = {'airports': airports}
+    return render(request, 'airports/index.html', context)
 
 
 def airport_info(request, airport_code):
@@ -16,4 +19,6 @@ def airport_info(request, airport_code):
     # Remember as we are only expecting one airport per code we should use get
     airport = Airport.objects.get(airport_code=airport_code)
     # Display the airport name and code
-    return HttpResponse('Showing info for airport: ' + airport.name + "- " + airport.airport_code)
+    context = {'airport': airport}
+    return render(request, 'airports/airport.html', context)
+    # return HttpResponse('Showing info for airport: ' + airport.name + "- " + airport.airport_code)
